b2a49e3fbfd2fbc2b30f489697fc9e62b174903f
kailany <emanuellymartinsales@gmail.com>
2023-10-19 22:32:57 -0300

    Aula 1 js

---
+++ teste.js
@@ -0,0 +1,51 @@
+//let pessoa1 = 'Henrique' //essa variavel pode ser reatribuida, logo, apos a segunda aplicação ela pode ser um novo valor
+
+//var pessoa2 = 'João'//mesma coisa do let mas ele nao é utilizado por ser antigo, logo não é uma boa pratica
+
+//const pessoa3 = 'Lana'//essa variavel uma vez declarada ela vai continuar ate o fim do cod, ela n pode ser reatribuida, ela é uma constante
+
+
+//EXEMPLO
+
+//let mensagem = 'Olá, essa é a mensagem da minha variavel';
+//console.log(mensagem);
+//mensagem = 'Essa é a nova mensagem da minha variavel'
+//onsole.log(mensagem);
+
+//float - numeros que contem pontos ou virgulas
+//int - numeros inteiros(não contem pontos ou virgulas)
+//bool: booleano - true(verdadeiro), false(falso)
+//string - do tipo de texto, ""
+
+let num1 = 15;
+let num2 = 20;
+
+//let soma = num1  + num2;
+//console.log(soma);
+
+//let multi = num1  * num2;
+//console.log(multi);
+
+//let divisao = num1  / num2;
+//console.log(divisao);
+
+//let sub = num1  - num2;
+//console.log(sub);
+
+
+//if - se
+//else - então
+
+// > maior que 
+// < menor que
+// >= maior ou igual
+// <= menor ou igual
+// === igual
+
+if(num1 > 10){
+    console.log('O numero 1 é maior do que 10')}
+
+else{
+    console.log('O numero 1 não é maior do que 10')}
+    
+
